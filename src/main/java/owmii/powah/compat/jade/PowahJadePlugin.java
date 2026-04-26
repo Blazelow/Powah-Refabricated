4748bccd6a3c6105d8b2537dccb3380a04fed641
Blazelow <bernardomaduro24@gmail.com>
2026-04-26 17:01:18 +0100

    Fix Jade duplicate UID crash: register once against AbstractEnergyBlock

--- src/main/java/owmii/powah/compat/jade/PowahJadePlugin.java
+++ src/main/java/owmii/powah/compat/jade/PowahJadePlugin.java
@@ -2,7 +2,7 @@ package owmii.powah.compat.jade;
 
 import net.minecraft.resources.ResourceLocation;
 import owmii.powah.Powah;
-import owmii.powah.block.Blcks;
+import owmii.powah.lib.block.AbstractEnergyBlock;
 import snownee.jade.api.IWailaClientRegistration;
 import snownee.jade.api.IWailaCommonRegistration;
 import snownee.jade.api.IWailaPlugin;
@@ -15,36 +15,13 @@ public class PowahJadePlugin implements IWailaPlugin {
 
     @Override
     public void register(IWailaCommonRegistration registration) {
-        // VarReg blocks — iterate all tiers
-        Blcks.SOLAR_PANEL.getAll().forEach(b -> registration.registerBlockDataProvider(PowahEnergyProvider.INSTANCE, b.getClass()));
-        Blcks.FURNATOR.getAll().forEach(b -> registration.registerBlockDataProvider(PowahEnergyProvider.INSTANCE, b.getClass()));
-        Blcks.MAGMATOR.getAll().forEach(b -> registration.registerBlockDataProvider(PowahEnergyProvider.INSTANCE, b.getClass()));
-        Blcks.THERMO_GENERATOR.getAll().forEach(b -> registration.registerBlockDataProvider(PowahEnergyProvider.INSTANCE, b.getClass()));
-        Blcks.ENERGY_CELL.getAll().forEach(b -> registration.registerBlockDataProvider(PowahEnergyProvider.INSTANCE, b.getClass()));
-        Blcks.ENDER_CELL.getAll().forEach(b -> registration.registerBlockDataProvider(PowahEnergyProvider.INSTANCE, b.getClass()));
-        Blcks.ENDER_GATE.getAll().forEach(b -> registration.registerBlockDataProvider(PowahEnergyProvider.INSTANCE, b.getClass()));
-        Blcks.ENERGIZING_ROD.getAll().forEach(b -> registration.registerBlockDataProvider(PowahEnergyProvider.INSTANCE, b.getClass()));
-        Blcks.REACTOR.getAll().forEach(b -> registration.registerBlockDataProvider(PowahEnergyProvider.INSTANCE, b.getClass()));
-        Blcks.ENERGY_CABLE.getAll().forEach(b -> registration.registerBlockDataProvider(PowahEnergyProvider.INSTANCE, b.getClass()));
-        Blcks.PLAYER_TRANSMITTER.getAll().forEach(b -> registration.registerBlockDataProvider(PowahEnergyProvider.INSTANCE, b.getClass()));
-        Blcks.ENERGY_HOPPER.getAll().forEach(b -> registration.registerBlockDataProvider(PowahEnergyProvider.INSTANCE, b.getClass()));
-        Blcks.ENERGY_DISCHARGER.getAll().forEach(b -> registration.registerBlockDataProvider(PowahEnergyProvider.INSTANCE, b.getClass()));
+        // Register once against the common superclass — Jade's hierarchy lookup
+        // automatically applies this to all subclasses (SolarBlock, FurnatorBlock, etc.)
+        registration.registerBlockDataProvider(PowahEnergyProvider.INSTANCE, AbstractEnergyBlock.class);
     }
 
     @Override
     public void registerClient(IWailaClientRegistration registration) {
-        Blcks.SOLAR_PANEL.getAll().forEach(b -> registration.registerBlockComponent(PowahEnergyProvider.INSTANCE, b.getClass()));
-        Blcks.FURNATOR.getAll().forEach(b -> registration.registerBlockComponent(PowahEnergyProvider.INSTANCE, b.getClass()));
-        Blcks.MAGMATOR.getAll().forEach(b -> registration.registerBlockComponent(PowahEnergyProvider.INSTANCE, b.getClass()));
-        Blcks.THERMO_GENERATOR.getAll().forEach(b -> registration.registerBlockComponent(PowahEnergyProvider.INSTANCE, b.getClass()));
-        Blcks.ENERGY_CELL.getAll().forEach(b -> registration.registerBlockComponent(PowahEnergyProvider.INSTANCE, b.getClass()));
-        Blcks.ENDER_CELL.getAll().forEach(b -> registration.registerBlockComponent(PowahEnergyProvider.INSTANCE, b.getClass()));
-        Blcks.ENDER_GATE.getAll().forEach(b -> registration.registerBlockComponent(PowahEnergyProvider.INSTANCE, b.getClass()));
-        Blcks.ENERGIZING_ROD.getAll().forEach(b -> registration.registerBlockComponent(PowahEnergyProvider.INSTANCE, b.getClass()));
-        Blcks.REACTOR.getAll().forEach(b -> registration.registerBlockComponent(PowahEnergyProvider.INSTANCE, b.getClass()));
-        Blcks.ENERGY_CABLE.getAll().forEach(b -> registration.registerBlockComponent(PowahEnergyProvider.INSTANCE, b.getClass()));
-        Blcks.PLAYER_TRANSMITTER.getAll().forEach(b -> registration.registerBlockComponent(PowahEnergyProvider.INSTANCE, b.getClass()));
-        Blcks.ENERGY_HOPPER.getAll().forEach(b -> registration.registerBlockComponent(PowahEnergyProvider.INSTANCE, b.getClass()));
-        Blcks.ENERGY_DISCHARGER.getAll().forEach(b -> registration.registerBlockComponent(PowahEnergyProvider.INSTANCE, b.getClass()));
+        registration.registerBlockComponent(PowahEnergyProvider.INSTANCE, AbstractEnergyBlock.class);
     }
 }
